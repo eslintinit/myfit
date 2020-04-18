4dc9f8e194101f2dd7fa6f9e328a710cb260ad89
ignatif <ignatif@gmail.com>
2020-04-18 20:14:07 +0300

    fix meta

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import Header from 'components/Header'
 import Navigation from 'components/Navigation'
-import Meta from './Meta'
+import Meta from 'components/Meta'
 
 import * as S from './Layout.styled'
 
