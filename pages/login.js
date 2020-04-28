b3794e5f547feab2485d81621dbf409fe48e4d10
ignatif <ignatif@gmail.com>
2020-04-28 15:23:13 +0300

    login

--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import Back from 'public/icons/Back.svg'
 import Arrow from 'public/icons/Arrow.svg'
 import NoSee from 'public/icons/NoSee.svg'
@@ -5,15 +7,57 @@ import Email from 'public/icons/Email.svg'
 import Key from 'public/icons/Key.svg'
 
 import * as S from 'styles/pages/login'
+import { GREY, BLACK } from 'styles/colors'
 
 
-export default () => (
-  <div>
-    <S.Bg>
-      <S.NavigationBar>
-        <Back />
-      </S.NavigationBar>
-      <S.InfoBlock></S.InfoBlock>
-    </S.Bg>
-  </div>
-)
+export default () => {
+  const [text, setText] = useState('')
+
+  const login = () => {
+    alert('Thanks. We will contact you shortly')
+  }
+
+  return (
+    <div>
+      <S.Bg>
+        <S.NavigationBar>
+          <Back />
+        </S.NavigationBar>
+        <S.InfoBlock>
+          <S.Caption>
+            <S.TextBold>Wellcome back, Susie Little</S.TextBold>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+              <img src='https://i.imgur.com/AC7RyMh.png' />
+              <div style={{ display: 'flex', flexDirection: 'row', marginTop: '8px' }}>
+                <S.Text>Change Account</S.Text>
+                <Arrow style={{ transform: 'rotate(270deg)', marginLeft: '4px' }}/>
+              </div>
+            </div>
+          </S.Caption>
+          <S.Text>Email</S.Text>
+          <S.Email>
+            <Email />
+            <S.Input
+              placeholder="myfit@gmailcom"
+            />
+          </S.Email>
+          <S.Text>Password</S.Text>
+          <S.Password>
+            <Key />
+            <S.Input
+              placeholder="Input password"
+              type="password"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            <NoSee />
+          </S.Password>
+          <S.RedText>Forgot password?</S.RedText>
+          <S.Login onClick={login} active={text !== ''}>
+            Login
+          </S.Login>
+        </S.InfoBlock>
+      </S.Bg>
+    </div>
+  )
+}
