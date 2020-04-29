f0df9a7a6d36edc94d05322866ea613ef2ddaf98
ignatif <ignatif@gmail.com>
2020-04-29 14:31:21 +0300

    sign up

--- pages/sign_up.js
+++ pages/sign_up.js
@@ -1,5 +1,7 @@
+import { useState } from 'react'
+
 import Back from 'public/icons/Back.svg'
-import NoSee from 'public/icons/NoSee.svg'
+import NoSee from 'public/icons/NoSeeG.svg'
 import Email from 'public/icons/Email.svg'
 import Key from 'public/icons/Key.svg'
 import Info from 'public/icons/Info.svg'
@@ -10,13 +12,71 @@ import Profile from 'public/icons/Profile.svg'
 import * as S from 'styles/pages/sign_up'
 
 
-export default () => (
-  <div>
-    <S.Bg>
-      <S.NavigationBar>
-        <Back />
-      </S.NavigationBar>
-      <S.InfoBlock></S.InfoBlock>
-    </S.Bg>
-  </div>
-)
+export default () => {
+  const [text, setText] = useState('')
+
+  const login = () => {
+    alert('Thanks. We will contact you shortly')
+  }
+
+  return (
+    <div>
+      <S.Bg>
+        <S.NavigationBar>
+          <Back />
+        </S.NavigationBar>
+        <S.InfoBlock>
+        <div style={{display: 'flex', flexDirection: 'row', marginBottom: '16px'}}>
+          <S.Line></S.Line>
+          <S.Line></S.Line>
+          <S.Line></S.Line>
+          <S.Line></S.Line>
+        </div>
+          <S.Caption>
+            <S.TextBold>New Account</S.TextBold>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+              <Photo style={{ marginBottom: '8px'}}/>
+              <S.Text>Upload picture</S.Text>
+            </div>
+          </S.Caption>
+          <S.Text>Full name</S.Text>
+          <S.Field>
+            <Profile />
+            <S.Input
+              placeholder="Enter your name"
+            />
+          </S.Field>
+          <S.Text>Email</S.Text>
+          <S.Field>
+            <Email />
+            <S.Input
+              placeholder="Enter email"
+            />
+          </S.Field>
+          <S.Text>Password</S.Text>
+          <S.Field>
+            <Key />
+            <S.Input
+              placeholder="Create password"
+              type="password"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            <NoSee />
+          </S.Field>
+          <S.Text>Authentication Cod</S.Text>
+          <S.Field>
+            <Lock />
+            <S.Input
+              placeholder="Enter Code"
+            />
+            <Info />
+          </S.Field>
+          <S.Login onClick={login} active={text !== ''}>
+            Create Account
+          </S.Login>
+        </S.InfoBlock>
+      </S.Bg>
+    </div>
+  )
+}
--- styles/pages/sign_up.js
+++ styles/pages/sign_up.js
@@ -7,18 +7,97 @@ import {
   PRIMARY,
 } from 'styles/colors'
 
-
- export const Bg = styled.div`
-  background-image: url(https://i.imgur.com/zRrcCEH.png);
-  height: 50vh;
+export const Bg = styled.div`
+  background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.33) 0%, rgba(0, 0, 0, 0.5) 100%),
+  url(https://i.imgur.com/zRrcCEH.png);
+  height: 100vh;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `
 
 export const NavigationBar = styled.div`
   height: 44px;
-  margin-left: 16px;
+  padding: 16px;
 `
 
 export const InfoBlock = styled.div`
   border-radius: 20px 20px 0px 0px;
   background: #FFFFFF;
+  padding: 24px 16px;
+  display: flex;
+  flex-direction: column;
+`
+ export const Line = styled.hr`
+  height: 2px;
+  background: ${PRIMARY};
+  width: 22vw;
+  border-radius: 4px;
+  border: none;
+ `
+
+export const Caption = styled.div`
+  margin-bottom: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const TextBold = styled.div`
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 36px;
+  color: BLACK;
+  width: 20vw;
+`
+
+export const Text = styled.p`
+  font-weight: bold;
+  font-size: 12px;
+  line-height: 16px;
+  color: ${DARK_GREY};
+`
+
+export const RedText = styled(Text)`
+  color: ${PRIMARY};
+  align-self: flex-end;
+`
+
+export const Field = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border-bottom: 1.5px solid rgba(189, 189, 189, 0.25);
+  margin-bottom: 24px;
+`
+
+export const Input = styled.input`
+border: none;
+font-weight: normal;
+font-size: 14px;
+line-height: 18px;
+color: ${BLACK};
+margin: 15px 16px;
+outline: none;
+flex-grow: 1;
+`
+
+export const Login = styled.div`
+  border-radius: 8px;
+  padding: 15px 16px;
+  margin-top: 40px;
+  text-align: center;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 18px;
+  color: white;
+  ${(props) =>
+    props.active
+      ? `
+    background: ${PRIMARY};
+  `
+      : `
+    background: rgba(250, 69, 4, 0.2);
+  `}
 `
