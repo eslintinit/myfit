c115d99159ea4adacfb337598a986864088bb525
ignatif <ignatif@gmail.com>
2020-04-30 14:56:44 +0300

    favorites cha cha cha

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -15,17 +15,18 @@ const routeTitleMap = {
 
 const Header = ({ showSidebar, setShowSidebar }) => {
   const { route } = useRouter()
-  const isHome = route === '/'
+  const showLogo =
+    route === '/' || route === '/favorites' || route === '/combos'
 
   return (
     <S.Header>
       {showSidebar ? <div /> : <Menu onClick={() => setShowSidebar(true)} />}
-      {isHome ? (
+      {showLogo ? (
         <Logo />
       ) : (
         <S.HeaderTitle>{routeTitleMap[route]}</S.HeaderTitle>
       )}
-      {isHome ? <Notifications /> : <div style={{ width: 27 }} />}
+      {showLogo ? <Notifications /> : <div style={{ width: 27 }} />}
     </S.Header>
   )
 }
---
+++ styles/pages/favorites.js
@@ -0,0 +1,85 @@
+import styled from 'styled-components'
+
+import {
+  BLACK,
+  DARK_GREY,
+  GREY,
+  PRIMARY,
+  SOFT_ORANGE,
+} from 'styles/colors'
+
+export const Cards = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-top: 24px;
+`
+
+export const Card = styled.div`
+  margin-bottom: 24px;
+`
+
+export const Caption = styled.div`
+  padding: 12px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 8px;
+`
+export const CaptionOne = styled(Caption)`
+  background: ${SOFT_ORANGE};
+`
+
+export const CaptionTwo = styled(Caption)`
+  background: #F8F8F8;
+`
+
+export const HeadText = styled.h1`
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 32px;
+  color: ${BLACK};
+`
+export const TinyText = styled.p`
+  font-weight: 600;
+  font-size: 10px;
+  line-height: 14px;
+  color: ${DARK_GREY};
+`
+
+export const Exercise = styled.div`
+  margin: 16px 12px 0px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+export const Info = styled.div`
+  flex-grow: 1;
+`
+
+export const Picture = styled.img`
+  width: 48px;
+  height: 48px;
+  margin-right: 16px;
+`
+
+export const TextBold = styled.h3`
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 18px;
+  color: ${BLACK};
+  margin-bottom: 6px;
+`
+
+export const Time = styled.div`
+  display: flex;
+`
+export const Min = styled.p`
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 16px;
+  color: ${DARK_GREY};
+  margin-left: 4px;
+`
