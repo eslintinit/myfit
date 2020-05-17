08ad577e1db626f2f5e7482641f0a417239d8503
ignatif <ignatif@gmail.com>
2020-05-17 10:55:50 +0300

    notification

--- styles/pages/notifications.js
+++ styles/pages/notifications.js
@@ -13,7 +13,7 @@ export const NavigationBar = styled.div`
   align-items: center;
   justify-content: space-between;
   padding: 8px 0px;
-  margin-bottom: 24px;
+  margin-bottom: 8px;
 `
 
 export const TextBold = styled.p`
@@ -23,26 +23,56 @@ export const TextBold = styled.p`
   color: white;
 `
 
-export const Notifications = styled.div``
-
-export const Card = styled.div`
-  border-bpttom: 1.5px solid rgba(189, 189, 189, 0.15);
+export const Notification = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-
+  padding: 16px 0px;
+  border-bottom: 1.5px solid rgba(189, 189, 189, 0.15);
 `
 
-export const Text= styled.p`
+export const Text = styled.p`
   font-weight: normal;
   font-size: 12px;
   line-height: 16px;
   color: ${GREY};
+  margin-left: 16px;
+  margin-right: 24px;
+  flex-grow: 1;
+  width: 40vw;
 `
 
-export const Time= styled.p`
+export const Highlight = styled.span`
+  color: white;
+  font-weight: bold;
+`
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 18vw;
+  justify-content: space-between;
+`
+
+export const Time = styled.p`
   font-weight: normal;
   font-size: 12px;
   line-height: 15px;
   color: ${DARK_GREY};
+  margin-right: 16px;
+`
+
+export const EllipseGrey = styled.div`
+  width: 3px;
+  height: 3px;
+  background: ${DARK_GREY};
+  border-radius: 50%;
+`
+
+export const EllipseRed = styled.div`
+  width: 6px;
+  height: 6px;
+  background: ${PRIMARY};
+  border-radius: 50%;
 `
