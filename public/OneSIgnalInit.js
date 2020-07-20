72be350fdbec55f474a9ec6d48d17d1c46802632
jjjyk <gmodhl@gmail.com>
2020-07-20 19:58:11 +0800

    push da tempo

--- public/OneSIgnalInit.js
+++ public/OneSIgnalInit.js
@@ -1,4 +1,4 @@
-var OneSignal = window.OneSignal || [];
+window.OneSignal = window.OneSignal || [];
               OneSignal.push(function() {
                 OneSignal.init({
                   appId: "da9b4f29-34d8-4e4b-9c57-522d153b729b",
