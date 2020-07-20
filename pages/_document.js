46ea95c884a8ee9a3cc56abd4a06ec3ef7047c15
jjjyk <gmodhl@gmail.com>
2020-07-20 20:39:56 +0800

    push push push

--- pages/_document.js
+++ pages/_document.js
@@ -53,6 +53,14 @@ export default class MyDocument extends Document {
             async=""
             src="../public/OneSIgnalInit.js"
           ></script>
+          <script>
+            window.OneSignal = window.OneSignal || [];
+            OneSignal.push(function() {'{'}
+            OneSignal.init({'{'}
+            appId: "da9b4f29-34d8-4e4b-9c57-522d153b729b",
+            {'}'});
+            {'}'});
+          </script>
           <link
             href="https://fonts.googleapis.com/css2?family=Muli:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
