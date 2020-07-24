49dd391fa59179f130189e709c1d6d891ba21edd
jjjuk <gmodhl67@gmail.com>
2020-07-25 01:46:30 +0800

    trying to run pushes

--- public/OneSIgnalInit.js
+++ public/OneSIgnalInit.js
@@ -1,16 +1,19 @@
+if (process.browser) {
+  
+  var OneSignal = window.OneSignal || [];
 
-var OneSignal = window.OneSignal || [];
 
-
-OneSignal.push(function() {
-  OneSignal.init({
-    appId: "da9b4f29-34d8-4e4b-9c57-522d153b729b",
-    notifyButton: {
-      enable: true,
-    },
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "da9b4f29-34d8-4e4b-9c57-522d153b729b",
+      notifyButton: {
+        enable: true,
+      },
+    });
+    OneSignal.showNativePrompt();
   });
-  OneSignal.showNativePrompt();
-});
+}
+
 
 
 
