64fe299c9b751ee7c8996e1cc60763b3fe1636f8
jjjuk <gmodhl67@gmail.com>
2020-07-25 01:29:39 +0800

    trying to run pushes

--- public/OneSIgnalInit.js
+++ public/OneSIgnalInit.js
@@ -1,9 +1,6 @@
  
- console.log(JSON.stringify(window.OneSignal))
  
- 
- 
- export default window.OneSignal = window.OneSignal || [];
+var OneSignal = OneSignal || [];
               OneSignal.push(function() {
                 OneSignal.init({
                   appId: "da9b4f29-34d8-4e4b-9c57-522d153b729b",
