e0eec8a04eaf6ff8cb0cd0c04fdbbb56ba7b1e41
jjjuk <gmodhl67@gmail.com>
2020-07-25 00:31:42 +0800

    push push

--- public/OneSIgnalInit.js
+++ public/OneSIgnalInit.js
@@ -1,4 +1,4 @@
-window.OneSignal = window.OneSignal || [];
+ window.OneSignal = window.OneSignal || [];
               OneSignal.push(function() {
                 OneSignal.init({
                   appId: "da9b4f29-34d8-4e4b-9c57-522d153b729b",
@@ -6,4 +6,4 @@ window.OneSignal = window.OneSignal || [];
                     enable: true,
                   },
                 });
-              });
+              }); 
