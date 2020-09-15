36b0eea0a7ba40ae05fb3883c52487ff8abf138b
ignatif <ignatif@gmail.com>
2020-09-16 02:29:16 +0300

    blog link

--- components/blog/intro.js
+++ components/blog/intro.js
@@ -2,7 +2,9 @@ export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        <img src={`/icons/LogoBig.svg`} />
+        <a href="/">
+          <img src={`/icons/LogoBig.svg`} />
+        </a>
       </h1>
     </section>
   )
